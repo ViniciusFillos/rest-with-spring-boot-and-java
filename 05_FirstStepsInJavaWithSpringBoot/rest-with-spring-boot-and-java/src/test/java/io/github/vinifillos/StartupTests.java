8c7bc6f2e8591602fcba36af9028754a17b2a0aa
Vinicius Fillos <vinifillos@gmail.com>
2024-05-22 19:56:36 -0300

    Project created

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/io/github/vinifillos/StartupTests.java
@@ -0,0 +1,13 @@
+package io.github.vinifillos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
