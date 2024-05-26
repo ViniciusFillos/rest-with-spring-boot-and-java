c06e8e48c38f8e7e69bfc2828627159d93f177c0
Vinicius Fillos <vinifillos@gmail.com>
2024-05-26 18:19:21 -0300

    test: tests for mapper from person to dto and dto to person implemented

---
+++ rest-with-spring-boot-and-java/src/test/java/io/github/vinifillos/unitTests/mapper/DozerConverterTest.java
@@ -0,0 +1,99 @@
+package io.github.vinifillos.unitTests.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import io.github.vinifillos.mapper.ModelMapper;
+import io.github.vinifillos.model.Person;
+import io.github.vinifillos.model.dto.PersonDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.github.vinifillos.unitTests.mocks.MockPerson;
+
+public class DozerConverterTest {
+    
+    MockPerson inputObject;
+
+    @BeforeEach
+    public void setUp() {
+        inputObject = new MockPerson();
+    }
+
+    @Test
+    public void parseEntityToVOTest() {
+        PersonDto output = ModelMapper.parseObject(inputObject.mockEntity(), PersonDto.class);
+        assertEquals(Long.valueOf(0L), output.getId());
+        assertEquals("First Name Test0", output.getFirstName());
+        assertEquals("Last Name Test0", output.getLastName());
+        assertEquals("Addres Test0", output.getAddress());
+        assertEquals("Male", output.getGender());
+    }
+
+    @Test
+    public void parseEntityListToVOListTest() {
+        List<PersonDto> outputList = ModelMapper.parseListObjects(inputObject.mockEntityList(), PersonDto.class);
+        PersonDto outputZero = outputList.get(0);
+        
+        assertEquals(Long.valueOf(0L), outputZero.getId());
+        assertEquals("First Name Test0", outputZero.getFirstName());
+        assertEquals("Last Name Test0", outputZero.getLastName());
+        assertEquals("Addres Test0", outputZero.getAddress());
+        assertEquals("Male", outputZero.getGender());
+
+        PersonDto outputSeven = outputList.get(7);
+        
+        assertEquals(Long.valueOf(7L), outputSeven.getId());
+        assertEquals("First Name Test7", outputSeven.getFirstName());
+        assertEquals("Last Name Test7", outputSeven.getLastName());
+        assertEquals("Addres Test7", outputSeven.getAddress());
+        assertEquals("Female", outputSeven.getGender());
+
+        PersonDto outputTwelve = outputList.get(12);
+        
+        assertEquals(Long.valueOf(12L), outputTwelve.getId());
+        assertEquals("First Name Test12", outputTwelve.getFirstName());
+        assertEquals("Last Name Test12", outputTwelve.getLastName());
+        assertEquals("Addres Test12", outputTwelve.getAddress());
+        assertEquals("Male", outputTwelve.getGender());
+    }
+
+    @Test
+    public void parseVOToEntityTest() {
+        Person output = ModelMapper.parseObject(inputObject.mockVO(), Person.class);
+        assertEquals(Long.valueOf(0L), output.getId());
+        assertEquals("First Name Test0", output.getFirstName());
+        assertEquals("Last Name Test0", output.getLastName());
+        assertEquals("Addres Test0", output.getAddress());
+        assertEquals("Male", output.getGender());
+    }
+
+    @Test
+    public void parserVOListToEntityListTest() {
+        List<Person> outputList = ModelMapper.parseListObjects(inputObject.mockVOList(), Person.class);
+        Person outputZero = outputList.get(0);
+        
+        assertEquals(Long.valueOf(0L), outputZero.getId());
+        assertEquals("First Name Test0", outputZero.getFirstName());
+        assertEquals("Last Name Test0", outputZero.getLastName());
+        assertEquals("Addres Test0", outputZero.getAddress());
+        assertEquals("Male", outputZero.getGender());
+        
+        Person outputSeven = outputList.get(7);
+        
+        assertEquals(Long.valueOf(7L), outputSeven.getId());
+        assertEquals("First Name Test7", outputSeven.getFirstName());
+        assertEquals("Last Name Test7", outputSeven.getLastName());
+        assertEquals("Addres Test7", outputSeven.getAddress());
+        assertEquals("Female", outputSeven.getGender());
+        
+        Person outputTwelve = outputList.get(12);
+        
+        assertEquals(Long.valueOf(12L), outputTwelve.getId());
+        assertEquals("First Name Test12", outputTwelve.getFirstName());
+        assertEquals("Last Name Test12", outputTwelve.getLastName());
+        assertEquals("Addres Test12", outputTwelve.getAddress());
+        assertEquals("Male", outputTwelve.getGender());
+    }
+}
---
+++ rest-with-spring-boot-and-java/src/test/java/io/github/vinifillos/unitTests/mocks/MockPerson.java
@@ -0,0 +1,58 @@
+package io.github.vinifillos.unitTests.mocks;
+
+import io.github.vinifillos.model.Person;
+import io.github.vinifillos.model.dto.PersonDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class MockPerson {
+
+
+    public Person mockEntity() {
+        return mockEntity(0);
+    }
+    
+    public PersonDto mockVO() {
+        return mockVO(0);
+    }
+    
+    public List<Person> mockEntityList() {
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 14; i++) {
+            persons.add(mockEntity(i));
+        }
+        return persons;
+    }
+
+    public List<PersonDto> mockVOList() {
+        List<PersonDto> persons = new ArrayList<>();
+        for (int i = 0; i < 14; i++) {
+            persons.add(mockVO(i));
+        }
+        return persons;
+    }
+    
+    public Person mockEntity(Integer number) {
+        Person person = new Person();
+        person.setAddress("Addres Test" + number);
+        person.setFirstName("First Name Test" + number);
+        person.setGender(((number % 2)==0) ? "Male" : "Female");
+        person.setId(number.longValue());
+        person.setLastName("Last Name Test" + number);
+        return person;
+    }
+
+    public PersonDto mockVO(Integer number) {
+        PersonDto person = new PersonDto();
+        person.setAddress("Addres Test" + number);
+        person.setFirstName("First Name Test" + number);
+        person.setGender(((number % 2)==0) ? "Male" : "Female");
+        person.setId(number.longValue());
+        person.setLastName("Last Name Test" + number);
+        return person;
+    }
+
+}
