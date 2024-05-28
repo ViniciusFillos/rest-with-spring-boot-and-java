6caf2d2072e24b547f3ee9f44789a191faa79551
Vinicius Fillos <vinifillos@gmail.com>
2024-05-28 10:25:40 -0300

    migrations in database with flyway implemented

---
+++ rest-with-spring-boot-and-java/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO `person` VALUES
+(1,'Street Alfredo Kamisnki','Vinicius','Male','Fillos'),
+(2,'Street street','Pietra','Female','Canesso'),
+(4,'Moscou - India','Muhammad','Male','Gandhi'),
+(6,'Mvezo - South Africa','Nelson','Male','Mandela'),
+(7,'smiljan - Croatia','Nikola','Male','Tesla');
