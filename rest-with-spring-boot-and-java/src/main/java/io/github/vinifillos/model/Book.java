c4b1c26834eaa92f8a15a9af00e3a436aad68f6d
Vinicius Fillos <vinifillos@gmail.com>
2024-05-30 09:44:07 -0300

    refactor: names on db and migrations for books changed

--- rest-with-spring-boot-and-java/src/main/java/io/github/vinifillos/model/Book.java
+++ rest-with-spring-boot-and-java/src/main/java/io/github/vinifillos/model/Book.java
@@ -7,7 +7,7 @@ import java.util.Date;
 import java.util.Objects;
 
 @Entity
-@Table(name = "books")
+@Table(name = "book")
 public class Book implements Serializable {
 
     private static final long serialVersionUID = 1L;
