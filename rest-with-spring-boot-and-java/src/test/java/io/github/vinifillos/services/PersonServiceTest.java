0e91047752edcbb3dc8e52fce8cc5a3df22caf1f
Vinicius Fillos <vinifillos@gmail.com>
2024-05-29 21:51:07 -0300

    test: testing hateoas link generation on service with mockito and JUnit5

---
+++ rest-with-spring-boot-and-java/src/test/java/io/github/vinifillos/services/PersonServiceTest.java
@@ -0,0 +1,73 @@
+package io.github.vinifillos.services;
+
+import io.github.vinifillos.model.Person;
+import io.github.vinifillos.repositories.PersonRepository;
+import io.github.vinifillos.unitTests.mocks.MockPerson;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+class PersonServiceTest {
+
+    MockPerson input;
+
+    @InjectMocks
+    private PersonService service;
+
+    @Mock
+    PersonRepository personRepository;
+
+    @BeforeEach
+    void setUpMocks() {
+        input = new MockPerson();
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+        Person person = input.mockEntity(1);
+        person.setId(1L);
+        when(personRepository.findById(anyLong())).thenReturn(Optional.of(person));
+
+        var result = service.findById(1L);
+
+        assertNotNull(result);
+        assertNotNull(result.getKey());
+        assertNotNull(result.getLinks());
+        assertTrue(result.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
+        assertEquals("Addres Test1", result.getAddress());
+        assertEquals("First Name Test1", result.getFirstName());
+        assertEquals("Last Name Test1", result.getLastName());
+        assertEquals("Female", result.getGender());
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
